054c0da6f54a6298a0e7590d2ddb46d7366154ff
nb-git <niels-buchholz@web.de>
2017-09-03 21:03:37 +0200

    First commit

---
+++ src/main/java/de/hero/example/herodemo/HeroDemoApplication.java
@@ -0,0 +1,12 @@
+package de.hero.example.herodemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HeroDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HeroDemoApplication.class, args);
+	}
+}
---
+++ src/test/java/de/hero/example/herodemo/HeroDemoApplicationTests.java
@@ -0,0 +1,16 @@
+package de.hero.example.herodemo;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class HeroDemoApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
